ade5b9e274667ef73a0f7a3c5d8d181490a51571
Truman Kilen <t@kilen.me>
2023-07-13 22:08:52 -0500

    Build diff database

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,18 @@ use patternsleuth::{
 #[derive(clap::Subcommand)]
 enum Action {
     Functions(ActionFunctions),
+    Sym(ActionSym),
 }
 
 #[derive(Parser)]
 pub struct ActionFunctions {
+    exe: std::path::PathBuf,
+    other_exe: std::path::PathBuf,
+    address: Option<String>,
+}
+
+#[derive(Parser)]
+pub struct ActionSym {
     exe: std::path::PathBuf,
     other_exe: std::path::PathBuf,
     address: Option<String>,
@@ -390,6 +398,130 @@ mod fns {
         }
     }
 
+    fn serialize_hex<S>(v: &u64, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&format!("{:x}", v))
+    }
+    fn deserialize_hex<'de, D>(deserializer: D) -> Result<u64, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        use serde::de::Error;
+        let s: &str = serde::Deserialize::deserialize(deserializer)?;
+        u64::from_str_radix(s, 16).map_err(D::Error::custom)
+    }
+
+    #[derive(Debug, serde::Serialize, serde::Deserialize)]
+    struct DiffRecord {
+        #[serde(serialize_with = "serialize_hex", deserialize_with = "deserialize_hex")]
+        a_start: u64,
+        #[serde(serialize_with = "serialize_hex", deserialize_with = "deserialize_hex")]
+        a_end: u64,
+        #[serde(serialize_with = "serialize_hex", deserialize_with = "deserialize_hex")]
+        b_start: u64,
+        #[serde(serialize_with = "serialize_hex", deserialize_with = "deserialize_hex")]
+        b_end: u64,
+        symbol: Option<String>,
+        score: i32,
+    }
+
+    type Symbols = HashMap<u64, String>;
+    use pdb::{FallibleIterator, PdbInternalSectionOffset};
+
+    fn print_row(
+        symbols: &mut Symbols,
+        address_map: &pdb::AddressMap<'_>,
+        base_address: u64,
+        offset: PdbInternalSectionOffset,
+        name: pdb::RawString<'_>,
+    ) {
+        let name = name.to_string().to_string();
+        if let Some(rva) = offset.to_rva(address_map) {
+            //println!("{:x} {name}", rva.0 as u64 + base_address);
+            symbols.insert(rva.0 as u64 + base_address, name);
+        } else {
+            println!("failed to calc RVA for {}", name);
+        }
+        //println!( "{:x}\t{:x}\t{}", offset.section, offset.offset, name);
+    }
+
+    fn print_symbol(
+        symbols: &mut Symbols,
+        address_map: &pdb::AddressMap<'_>,
+        base_address: u64,
+        symbol: &pdb::Symbol<'_>,
+    ) -> pdb::Result<()> {
+        match symbol.parse()? {
+            pdb::SymbolData::Public(data) => {
+                print_row(symbols, address_map, base_address, data.offset, data.name);
+            }
+            //pdb::SymbolData::Data(data) => {
+            //print_row(data.offset, "data", data.name);
+            //}
+            pdb::SymbolData::Procedure(data) => {
+                print_row(symbols, address_map, base_address, data.offset, data.name);
+            }
+            _ => {}
+        }
+
+        Ok(())
+    }
+
+    fn walk_symbols(
+        symbols_map: &mut Symbols,
+        address_map: &pdb::AddressMap<'_>,
+        base_address: u64,
+        mut symbols: pdb::SymbolIter<'_>,
+    ) -> pdb::Result<()> {
+        println!("segment\toffset\tkind\tname");
+
+        while let Some(symbol) = symbols.next()? {
+            match print_symbol(symbols_map, address_map, base_address, &symbol) {
+                Ok(_) => (),
+                Err(e) => {
+                    //eprintln!("error printing symbol {:?}: {}", symbol, e);
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn dump_pdb<P: AsRef<Path>>(filename: P, base_address: u64) -> pdb::Result<Symbols> {
+        let mut symbols = Symbols::new();
+
+        let file = std::fs::File::open(filename)?;
+        let mut pdb = pdb::PDB::open(file)?;
+        let symbol_table = pdb.global_symbols()?;
+        let address_map = pdb.address_map()?;
+        println!("Global symbols:");
+        walk_symbols(
+            &mut symbols,
+            &address_map,
+            base_address,
+            symbol_table.iter(),
+        )?;
+
+        println!("Module private symbols:");
+        let dbi = pdb.debug_information()?;
+        let mut modules = dbi.modules()?;
+        while let Some(module) = modules.next()? {
+            println!("Module: {}", module.object_file_name());
+            let info = match pdb.module_info(&module)? {
+                Some(info) => info,
+                None => {
+                    println!("  no module info");
+                    continue;
+                }
+            };
+
+            walk_symbols(&mut symbols, &address_map, base_address, info.symbols()?)?;
+        }
+        Ok(symbols)
+    }
+
     fn read_exe<'data>(
         exe_data: &'data [u8],
         obj_file: &'data object::File,
@@ -424,6 +556,11 @@ mod fns {
         let functions = read_exe(&exe_data, &exe_obj)?;
         let other_functions = read_exe(&other_exe_data, &other_exe_obj)?;
 
+        let symbols = dump_pdb(
+            action.other_exe.with_extension("pdb"),
+            other_exe_obj.relative_address_base(),
+        )?;
+
         //println!("{:#x?}", other_functions.iter().filter(|f| f.range.contains(&0x1424c87b0)).collect_vec());
         //println!("{:#x?}", other_functions.iter().filter(|f| f.range.contains(&0x1424c87bf)).collect_vec());
         //return Ok(());
@@ -431,22 +568,12 @@ mod fns {
         println!("disassembling {}", action.exe.display());
         let fn_dis = functions
             .par_iter()
-            .map(|f| {
-                (
-                    f,
-                    &memory[f.range.clone()],
-                )
-            })
+            .map(|f| (f, &memory[f.range.clone()]))
             .collect::<Vec<_>>();
         println!("disassembling {}", action.other_exe.display());
         let other_fn_dis = other_functions
             .par_iter()
-            .map(|f| {
-                (
-                    f,
-                    &other_memory[f.range.clone()],
-                )
-            })
+            .map(|f| (f, &other_memory[f.range.clone()]))
             .collect::<Vec<_>>();
 
         const S: f32 = 30.0;
@@ -469,32 +596,60 @@ mod fns {
             println!("{} ({}): {}", k, inv_bin(*k), v.len());
         }
 
-        other_fn_dis.par_iter().for_each(|of| {
-            //if of.1.len() < 1000 { return; }
-            let m = bins
-                .get(&bin(&of.1))
-                .map(|f| f.iter())
-                .unwrap_or_default()
-                .map(|f| {
-                    let distance = sift4_bin::simple(&of.1, &f.1);
-                    (f.0.clone(), distance, &f.1)
-                })
-                .min_by_key(|f| f.1);
+        use indicatif::ParallelProgressIterator;
 
-            if let Some(m) = m {
-                //println!("{}", of.1);
-                println!("diff = {:>8} fn.len() = {:>8} match.len() = {:>8} fn addr = {:x} match addr = {:x}", m.1, of.1.len(), m.2.len(), m.0.range.start, of.0.range.start);
-                println!("wdiff -n <(objdump -M intel --no-show-raw-insn --no-addresses --start-address=0x{:x} --stop-address=0x{:x} --demangle -d {}) <(objdump -M intel --no-show-raw-insn --no-addresses --start-address=0x{:x} --stop-address=0x{:x} --demangle -d {}) | colordiff | less -SR",
-                    m.0.range.start, m.0.range.end, action.exe.display(), of.0.range.start, of.0.range.end, action.other_exe.display());
-            }
-            //distances.sort_by_key(|e| std::cmp::Reverse(e.0));
+        let records = other_fn_dis
+            .par_iter()
+            .progress_with_style(
+                indicatif::ProgressStyle::with_template(
+                    "[{elapsed_precise}] [{wide_bar}] {pos}/{len} ({eta})",
+                )
+                .unwrap(),
+            ) //.take(10)
+            .filter_map(|of| {
+                //if of.1.len() < 1000 { return; }
+                let m = bins
+                    .get(&bin(&of.1))
+                    .map(|f| f.iter())
+                    .unwrap_or_default()
+                    .map(|f| {
+                        let distance = sift4_bin::simple(&of.1, &f.1);
+                        (f.0.clone(), distance, &f.1)
+                    })
+                    .min_by_key(|f| f.1);
 
-            /*
-            for dist in distances {
-                println!("{:#x?}", dist);
-                println!("wdiff <(objdump -M intel --no-show-raw-insn --no-addresses --start-address=0x{:x} --stop-address=0x{:x} --demangle -d {}) <(objdump -M intel --no-show-raw-insn --no-addresses --start-address=0x{:x} --stop-address=0x{:x} --demangle -d {}) | colordiff | less -SR", function.range.start, function.range.end, action.exe.display(), dist.1.range.start, dist.1.range.end, action.other_exe.display());
-            }*/
-        });
+                if let Some(m) = m {
+                    //println!("{}", of.1);
+                    //println!("diff = {:>8} fn.len() = {:>8} match.len() = {:>8} fn addr = {:x} match addr = {:x}", m.1, of.1.len(), m.2.len(), m.0.range.start, of.0.range.start);
+                    //println!("wdiff -n <(objdump -M intel --no-show-raw-insn --no-addresses --start-address=0x{:x} --stop-address=0x{:x} --demangle -d {}) <(objdump -M intel --no-show-raw-insn --no-addresses --start-address=0x{:x} --stop-address=0x{:x} --demangle -d {}) | colordiff | less -SR",
+                    //m.0.range.start, m.0.range.end, action.exe.display(), of.0.range.start, of.0.range.end, action.other_exe.display());
+                    //println!("radiff2 -g 0x{:x},0x{:x} {} {}", m.0.range.start, /*m.0.range.end,*/ of.0.range.start, /*of.0.range.end,*/ action.exe.display(), action.other_exe.display());
+
+                    return Some(DiffRecord {
+                        a_start: m.0.range.start as u64,
+                        a_end: m.0.range.end as u64,
+                        b_start: of.0.range.start as u64,
+                        b_end: of.0.range.end as u64,
+                        symbol: symbols.get(&(of.0.range.start as u64)).cloned(),
+                        score: m.1,
+                    });
+                }
+                //distances.sort_by_key(|e| std::cmp::Reverse(e.0));
+
+                /*
+                for dist in distances {
+                    println!("{:#x?}", dist);
+                    println!("wdiff <(objdump -M intel --no-show-raw-insn --no-addresses --start-address=0x{:x} --stop-address=0x{:x} --demangle -d {}) <(objdump -M intel --no-show-raw-insn --no-addresses --start-address=0x{:x} --stop-address=0x{:x} --demangle -d {}) | colordiff | less -SR", function.range.start, function.range.end, action.exe.display(), dist.1.range.start, dist.1.range.end, action.other_exe.display());
+                }*/
+                None
+            })
+            .collect::<Vec<_>>();
+
+        let mut wtr = csv::Writer::from_path("diff.csv")?;
+        for record in records {
+            wtr.serialize(record)?;
+        }
+        wtr.flush()?;
 
         return Ok(());
 
@@ -554,6 +709,76 @@ mod fns {
 
         Ok(())
     }
+
+    pub fn sym(action: ActionSym) -> Result<(), Box<dyn Error>> {
+        let mut rdr = csv::Reader::from_path("diff.csv")?;
+        let records = rdr
+            .deserialize()
+            .map(|r| Ok(r?))
+            .collect::<Result<Vec<DiffRecord>>>()?;
+
+        fn find_record_containing(records: &[DiffRecord], address: u64) -> Option<&DiffRecord> {
+            records
+                .iter()
+                .find(|r| r.a_start <= address && r.a_end > address)
+        }
+
+        if let Some(address) = action.address {
+            if let Ok(address) = u64::from_str_radix(&address, 16) {
+                for record in records {
+                    if record.a_start <= address && record.a_end > address {
+                        println!("{:#?}", record);
+                    }
+                }
+            } else {
+                let file = std::fs::File::open(address)?;
+                let lines = std::io::BufReader::new(file).lines();
+
+                use regex::Regex;
+                let re = Regex::new(r#"(?<start>LogWindows: Error: \[Callstack\] 0x(?<address>[0-9a-fA-F]+) FSD-Win64-Shipping.exe!)(?<symbol>UnknownFunction)(?<end> \[\])"#).unwrap();
+                //let result = re.replace_all("Hello World!", "x");
+                //println!("{}", result); // => "xxxxx xxxxx!"
+
+                use regex::{Captures, Replacer};
+
+                struct SymbolResolver<'re>(&'re [DiffRecord]);
+
+                impl<'re> Replacer for SymbolResolver<'re> {
+                    fn replace_append(&mut self, caps: &Captures<'_>, dst: &mut String) {
+                        dst.push_str(&caps["start"]);
+                        if let Some(DiffRecord {
+                            a_start,
+                            a_end,
+                            score,
+                            symbol: Some(symbol),
+                            ..
+                        }) = find_record_containing(
+                            self.0,
+                            u64::from_str_radix(&caps["address"], 16).unwrap(),
+                        ) {
+                            dst.push_str(&format!(
+                                "{:x} @ {:_>5}/{:_>5} {}",
+                                a_start,
+                                score,
+                                a_end - a_start,
+                                symbol
+                            ));
+                        } else {
+                            dst.push_str(&caps["symbol"]);
+                        }
+                        dst.push_str(&caps["end"]);
+                    }
+                }
+
+                for line in lines {
+                    let line = line?;
+                    println!("{}", re.replace(&line, SymbolResolver(&records)));
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -561,6 +786,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     match cli.action {
         Some(Action::Functions(action)) => return fns::functions(action),
+        Some(Action::Sym(action)) => return fns::sym(action),
         None => {}
     }
 
