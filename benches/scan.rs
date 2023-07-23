a750f600199a6d1af51c4d646d13b82097745593
Truman Kilen <t@kilen.me>
2023-07-22 20:40:59 -0500

    Experimenting scan variant

--- benches/scan.rs
+++ benches/scan.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 fn gig(c: &mut Criterion) {
     use rand::prelude::*;
@@ -19,18 +19,200 @@ fn gig(c: &mut Criterion) {
     let result = patternsleuth::scanner::scan_memchr(&[(&(), &pattern)], 0, &data);
     assert_eq!(result, [(&(), size - needle.len())]);
 
-    c.bench_function("gig scan", |b| {
+    c.bench_function("one gig scan", |b| {
         b.iter(|| patternsleuth::scanner::scan(&[(&(), &pattern)], 0, &data))
     });
-    c.bench_function("gig scan_memchr", |b| {
+    c.bench_function("one gig scan_memchr", |b| {
         b.iter(|| patternsleuth::scanner::scan_memchr(&[(&(), &pattern)], 0, &data))
+    });
+    c.bench_function("one gig scan_simple", |b| {
+        b.iter(|| patternsleuth::scanner::scan_simple(&[(&(), &pattern)], 0, &data))
     });
 }
 
+fn gig_freq(c: &mut Criterion) {
+    use rand::prelude::*;
+    let size = 1024 * 1024 * 1024 / 10;
+    let mut data: Vec<u8> = vec![0; size];
+    let mut rng = rand::thread_rng();
+
+    let needle = b"\x01\x82\xdb\xdb\x2d\x32\x6f\x15\x11\x44\x54\xf4\xc8\xaa\xd1\x72\x53\x96\xa5\x7b\x22\x24\x94\x7f\xec\x28\xc7\xe0\x5e\xd4\xae\x39";
+
+    let pattern = patternsleuth::Pattern::new("01 82 db db 2d ?? 6f 15 ?? 44 54 f4 c8 aa d1 72 53 ?? a5 7b 22 24 94 7f ec 28 ?? e0 5e d4 ae 39").unwrap();
+
+    let mut group = c.benchmark_group("freq");
+
+    let steps = 10;
+    for freq in 0..steps + 1 {
+        let f = freq as f64 / steps as f64;
+        for d in data.iter_mut() {
+            *d = if rng.gen_bool(f) { 1 } else { 2 }
+        }
+
+        data[(size - needle.len())..].copy_from_slice(needle);
+
+        let result = patternsleuth::scanner::scan(&[(&(), &pattern)], 0, &data);
+        assert_eq!(result, [(&(), size - needle.len())]);
+
+        let result = patternsleuth::scanner::scan_memchr(&[(&(), &pattern)], 0, &data);
+        assert_eq!(result, [(&(), size - needle.len())]);
+
+        group.bench_with_input(BenchmarkId::new("ps", f), &f, |b, _size| {
+            b.iter(|| patternsleuth::scanner::scan(&[(&(), &pattern)], 0, &data))
+        });
+        group.bench_with_input(BenchmarkId::new("memchr", f), &f, |b, _size| {
+            b.iter(|| patternsleuth::scanner::scan_memchr(&[(&(), &pattern)], 0, &data))
+        });
+        group.bench_with_input(BenchmarkId::new("simple", f), &f, |b, _size| {
+            b.iter(|| patternsleuth::scanner::scan_simple(&[(&(), &pattern)], 0, &data))
+        });
+
+        /*
+        c.bench_function(&format!("freq gig scan = {:.2}", f), |b| {
+            b.iter(|| patternsleuth::scanner::scan(&[(&(), &pattern)], 0, &data))
+        });
+        c.bench_function(&format!("freq gig scan_memchr = {:.2}", f), |b| {
+            b.iter(|| patternsleuth::scanner::scan_memchr(&[(&(), &pattern)], 0, &data))
+        });
+        c.bench_function(&format!("freq gig scan_simple = {:.2}", f), |b| {
+            b.iter(|| patternsleuth::scanner::scan_simple(&[(&(), &pattern)], 0, &data))
+        });
+        */
+    }
+    group.finish();
+}
+
+fn multi(c: &mut Criterion) {
+    use rand::prelude::*;
+    let size = 1024 * 1024 * 1024 / 10;
+    let mut data: Vec<u8> = vec![0; size];
+    let mut rng = rand::thread_rng();
+
+    let patterns = (1..30)
+        .map(|i| {
+            //patternsleuth::Pattern::new(&format!("{i:02x} 82 db db 2d ?? 6f 15 ?? 44 54 f4 c8"))
+            patternsleuth::Pattern::new(&format!("01 82 db db 2d ?? 6f 15 ?? 44 54 f4 c8")).unwrap()
+        })
+        .collect::<Vec<_>>();
+    let pattern_counts = (1..patterns.len())
+        .map(|i| {
+            patterns
+                .iter()
+                .take(i)
+                .map(|p| (&(), p))
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("multi");
+    //group.sampling_mode(SamplingMode::Flat);
+
+    //let steps = 10;
+    //for freq in 0..steps + 1 {
+    //let f = freq as f64 / steps as f64;
+    let f = 0.1;
+    for d in data.iter_mut() {
+        //*d = if rng.gen_bool(f) { rng.gen_range(1..30) } else { 0 }
+        *d = if rng.gen_bool(f) { 1 } else { 0 }
+    }
+
+    for p in pattern_counts {
+        let size = p.len();
+        group.bench_with_input(
+            BenchmarkId::new("patternsleuth", size),
+            &size,
+            |b, _size| b.iter(|| patternsleuth::scanner::scan(&p, 0, &data)),
+        );
+        group.bench_with_input(BenchmarkId::new("memchr", size), &size, |b, _size| {
+            b.iter(|| patternsleuth::scanner::scan_memchr(&p, 0, &data))
+        });
+        group.bench_with_input(BenchmarkId::new("simple", size), &size, |b, _size| {
+            b.iter(|| patternsleuth::scanner::scan_simple(&p, 0, &data))
+        });
+    }
+
+    /*
+    c.bench_function(&format!("freq gig scan = {:.2}", f), |b| {
+        b.iter(|| patternsleuth::scanner::scan(&[(&(), &pattern)], 0, &data))
+    });
+    c.bench_function(&format!("freq gig scan_memchr = {:.2}", f), |b| {
+        b.iter(|| patternsleuth::scanner::scan_memchr(&[(&(), &pattern)], 0, &data))
+    });
+    c.bench_function(&format!("freq gig scan_simple = {:.2}", f), |b| {
+        b.iter(|| patternsleuth::scanner::scan_simple(&[(&(), &pattern)], 0, &data))
+    });
+    */
+    //}
+    group.finish();
+}
+
+fn fsd(c: &mut Criterion) {
+    let data = std::fs::read("games/FSD/FSD-Win64-Shipping.exe").unwrap();
+
+    let raw_patterns = patternsleuth::patterns::get_patterns()
+        .unwrap()
+        .into_iter()
+        .map(|c| c.pattern)
+        .collect::<Vec<_>>();
+    let id_patterns = raw_patterns.iter().map(|p| (&(), p)).collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group("fsd");
+
+    let t = 5;
+    for f in 0..t {
+        let size = (raw_patterns.len() as f64 * f as f64 / t as f64).round() as usize;
+        let p = &id_patterns[0..size];
+        group.bench_with_input(
+            BenchmarkId::new("patternsleuth", size),
+            &size,
+            |b, _size| b.iter(|| patternsleuth::scanner::scan(&p, 0, &data)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("patternsleuth_lookup", size),
+            &size,
+            |b, _size| b.iter(|| patternsleuth::scanner::scan_lookup(&p, 0, &data)),
+        );
+        group.bench_with_input(BenchmarkId::new("memchr", size), &size, |b, _size| {
+            b.iter(|| patternsleuth::scanner::scan_memchr(&p, 0, &data))
+        });
+        group.bench_with_input(
+            BenchmarkId::new("memchr_lookup", size),
+            &size,
+            |b, _size| b.iter(|| patternsleuth::scanner::scan_memchr_lookup(&p, 0, &data)),
+        );
+        group.bench_with_input(BenchmarkId::new("simple", size), &size, |b, _size| {
+            b.iter(|| patternsleuth::scanner::scan_simple(&p, 0, &data))
+        });
+        group.bench_with_input(
+            BenchmarkId::new("simple_batched", size),
+            &size,
+            |b, _size| b.iter(|| patternsleuth::scanner::scan_simple_batched(&p, 0, &data)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("simple_batched_lookup", size),
+            &size,
+            |b, _size| b.iter(|| patternsleuth::scanner::scan_simple_batched_lookup(&p, 0, &data)),
+        );
+    }
+
+    group.finish();
+}
+
+/*
 criterion_group! {
     name = bench1;
     config = Criterion::default().sample_size(30);
     targets = gig
 }
+criterion_group! {
+    name = bench2;
+    config = Criterion::default().sample_size(30);
+    targets = gig_freq
+}
+criterion_main!(bench1, bench2);
+*/
 
-criterion_main!(bench1);
+criterion_group!(bench1, gig_freq);
+criterion_group!(bench2, multi);
+criterion_group!(bench3, fsd);
+criterion_main!(bench1, bench2, bench3);
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,4 +1,7 @@
-use std::simd::{SimdPartialEq, ToBitMask};
+use std::{
+    collections::HashMap,
+    simd::{SimdPartialEq, ToBitMask},
+};
 
 use super::Pattern;
 
@@ -39,18 +42,18 @@ pub fn scan<'id, ID: Sync>(
     }
 
     // middle
-    let batch_size = (middle.len()
+    let chunk_size = (middle.len()
         / std::thread::available_parallelism().unwrap_or(std::num::NonZeroUsize::new(1).unwrap()))
     .max(1);
-    let batches: Vec<_> = middle.chunks(batch_size).enumerate().collect();
+    let chunks: Vec<_> = middle.chunks(chunk_size).enumerate().collect();
     matches.append(
-        &mut batches
+        &mut chunks
             .par_iter()
-            .map(|(index, batch)| {
+            .map(|(index, chunk)| {
                 let mut matches = vec![];
-                let offset = index * batch_size;
+                let offset = index * chunk_size;
 
-                for (i, chunk) in batch.iter().enumerate() {
+                for (i, chunk) in chunk.iter().enumerate() {
                     for ((id, p), f) in &first {
                         let mut mask = f.simd_eq(*chunk).to_bitmask();
 
@@ -62,6 +65,96 @@ pub fn scan<'id, ID: Sync>(
 
                             if p.is_match(data, j) {
                                 matches.push((*id, p.compute_result(data, base_address, j)));
+                            }
+                        }
+                    }
+                }
+                matches
+            })
+            .flatten()
+            .collect(),
+    );
+
+    // suffix
+    let start = prefix.len() + middle.len() * LANES;
+    for (id, p) in patterns {
+        for i in start..start + suffix.len().saturating_sub(p.sig.len() - 1) {
+            if p.is_match(data, i) {
+                matches.push((*id, base_address + i));
+            }
+        }
+    }
+
+    matches
+}
+
+pub fn scan_lookup<'id, ID: Sync>(
+    patterns: &[(&'id ID, &Pattern)],
+    base_address: usize,
+    data: &[u8],
+) -> Vec<(&'id ID, usize)> {
+    use rayon::prelude::*;
+
+    if patterns.is_empty() {
+        return vec![];
+    }
+
+    const LANES: usize = 16;
+
+    let max = patterns.iter().map(|(_id, p)| p.sig.len()).max().unwrap();
+
+    let mut bins: HashMap<u8, Vec<_>> = Default::default();
+    for p in patterns {
+        bins.entry(p.1.sig[0]).or_default().push(p);
+    }
+
+    let first: Vec<_> = bins
+        .into_iter()
+        .map(|(first, patterns)| (std::simd::Simd::splat(first), patterns))
+        .collect();
+
+    // split data for simd
+    // cut middle short such that even the longest pattern doesn't have to bounds check
+    let (prefix, middle, _suffix) = data[0..data.len().saturating_sub(max)].as_simd::<LANES>();
+    let suffix = &data[prefix.len() + middle.len() * LANES..data.len()];
+
+    let mut matches = vec![];
+
+    // prefix
+    for (id, p) in patterns {
+        for i in 0..prefix.len().min(data.len().saturating_sub(p.sig.len() - 1)) {
+            if p.is_match(data, i) {
+                matches.push((*id, base_address + i));
+            }
+        }
+    }
+
+    // middle
+    let chunk_size = (middle.len()
+        / std::thread::available_parallelism().unwrap_or(std::num::NonZeroUsize::new(1).unwrap()))
+    .max(1);
+    let chunks: Vec<_> = middle.chunks(chunk_size).enumerate().collect();
+    matches.append(
+        &mut chunks
+            .par_iter()
+            .map(|(index, chunk)| {
+                let mut matches = vec![];
+                let offset = index * chunk_size;
+
+                for (i, chunk) in chunk.iter().enumerate() {
+                    for (f, patterns) in &first {
+                        let mut mask = f.simd_eq(*chunk).to_bitmask();
+
+                        while mask != 0 {
+                            let next = mask.trailing_zeros();
+                            mask ^= 1 << next;
+
+                            let j = prefix.len() + (offset + i) * LANES + next as usize;
+
+                            for (id, p) in patterns {
+                                if p.is_match(data, j) {
+                                    matches.push((*id, p.compute_result(data, base_address, j)));
+                                }
                             }
                         }
                     }
@@ -124,6 +217,233 @@ pub fn scan_memchr<'id, ID: Sync>(
     matches
 }
 
+pub fn scan_memchr_lookup<'id, ID: Sync + std::fmt::Debug>(
+    patterns: &[(&'id ID, &Pattern)],
+    base_address: usize,
+    data: &[u8],
+) -> Vec<(&'id ID, usize)> {
+    use rayon::prelude::*;
+
+    if patterns.is_empty() {
+        return vec![];
+    }
+
+    let mut bins: HashMap<u8, Vec<_>> = Default::default();
+    for p in patterns {
+        bins.entry(p.1.sig[0]).or_default().push(p);
+    }
+
+    let max = patterns.iter().map(|(_id, p)| p.sig.len()).max().unwrap();
+
+    // cut middle short such that even the longest pattern doesn't have to bounds check
+    let middle = &data[0..data.len().saturating_sub(max)];
+
+    let mut matches = vec![];
+
+    // middle
+    let chunk_size = (middle.len()
+        / std::thread::available_parallelism().unwrap_or(std::num::NonZeroUsize::new(1).unwrap()))
+    .max(1);
+    let chunks: Vec<_> = middle.chunks(chunk_size).enumerate().collect();
+    matches.append(
+        &mut chunks
+            .par_iter()
+            .map(|(index, chunk)| {
+                let mut matches = vec![];
+                let offset = index * chunk_size;
+
+                for (first, patterns) in &bins {
+                    for i in memchr::memchr_iter(*first, chunk) {
+                        let j = offset + i;
+                        for (id, p) in patterns {
+                            if p.is_match(data, j) {
+                                matches.push((*id, p.compute_result(data, base_address, j)));
+                            }
+                        }
+                    }
+                }
+                matches
+            })
+            .flatten()
+            .collect(),
+    );
+
+    // suffix
+    let start = middle.len();
+    for (id, p) in patterns {
+        for i in start..start + (data.len() - middle.len()).saturating_sub(p.sig.len() - 1) {
+            if p.is_match(data, i) {
+                matches.push((*id, base_address + i));
+            }
+        }
+    }
+
+    matches
+}
+
+pub fn scan_simple<'id, ID: Sync>(
+    patterns: &[(&'id ID, &Pattern)],
+    base_address: usize,
+    data: &[u8],
+) -> Vec<(&'id ID, usize)> {
+    use rayon::prelude::*;
+
+    let mut matches = vec![];
+
+    for (id, pattern) in patterns {
+        let first_byte_data = &data[0..data.len().saturating_sub(pattern.sig.len() - 1)];
+        let chunk_size = (first_byte_data.len()
+            / std::thread::available_parallelism()
+                .unwrap_or(std::num::NonZeroUsize::new(1).unwrap()))
+        .max(1);
+
+        let chunks: Vec<_> = first_byte_data.chunks(chunk_size).enumerate().collect();
+        matches.append(
+            &mut chunks
+                .par_iter()
+                .map(|(chunk_index, chunk)| {
+                    let mut matches = vec![];
+                    let offset = chunk_index * chunk_size;
+
+                    for i in 0..chunk.len() {
+                        let j = offset + i;
+                        if pattern.is_match(data, j) {
+                            matches.push((*id, pattern.compute_result(data, base_address, j)));
+                        }
+                    }
+                    matches
+                })
+                .flatten()
+                .collect::<Vec<_>>(),
+        );
+    }
+    matches
+}
+
+pub fn scan_simple_batched<'id, ID: Sync>(
+    patterns: &[(&'id ID, &Pattern)],
+    base_address: usize,
+    data: &[u8],
+) -> Vec<(&'id ID, usize)> {
+    use rayon::prelude::*;
+
+    if patterns.is_empty() {
+        return vec![];
+    }
+
+    let max = patterns.iter().map(|(_id, p)| p.sig.len()).max().unwrap();
+    let first: Vec<_> = patterns.iter().map(|p| p.1.sig[0]).collect();
+
+    // cut middle short such that even the longest pattern doesn't have to bounds check
+    let middle = &data[0..data.len().saturating_sub(max)];
+
+    let mut matches = vec![];
+
+    // middle
+    let chunk_size = (middle.len()
+        / std::thread::available_parallelism().unwrap_or(std::num::NonZeroUsize::new(1).unwrap()))
+    .max(1);
+    let chunks: Vec<_> = middle.chunks(chunk_size).enumerate().collect();
+    matches.append(
+        &mut chunks
+            .par_iter()
+            .map(|(index, chunk)| {
+                let mut matches = vec![];
+                let offset = index * chunk_size;
+
+                for (i, _) in chunk.iter().enumerate() {
+                    for (pattern_index, first_byte) in first.iter().enumerate() {
+                        let j = offset + i;
+
+                        if data[j] == *first_byte {
+                            let (id, p) = &patterns[pattern_index];
+                            if p.is_match(data, j) {
+                                matches.push((*id, p.compute_result(data, base_address, j)));
+                            }
+                        }
+                    }
+                }
+                matches
+            })
+            .flatten()
+            .collect(),
+    );
+
+    // suffix
+    let start = middle.len();
+    for (id, p) in patterns {
+        for i in start..start + (data.len() - middle.len()).saturating_sub(p.sig.len() - 1) {
+            if p.is_match(data, i) {
+                matches.push((*id, base_address + i));
+            }
+        }
+    }
+
+    matches
+}
+
+pub fn scan_simple_batched_lookup<'id, ID: Sync + std::fmt::Debug>(
+    patterns: &[(&'id ID, &Pattern)],
+    base_address: usize,
+    data: &[u8],
+) -> Vec<(&'id ID, usize)> {
+    use rayon::prelude::*;
+
+    if patterns.is_empty() {
+        return vec![];
+    }
+
+    let max = patterns.iter().map(|(_id, p)| p.sig.len()).max().unwrap();
+
+    let mut bins: Vec<Vec<(&ID, &Pattern)>> = (0..0x100).map(|_| vec![]).collect();
+    for pattern in patterns {
+        bins[pattern.1.sig[0] as usize].push(*pattern);
+    }
+
+    // cut middle short such that even the longest pattern doesn't have to bounds check
+    let middle = &data[0..data.len().saturating_sub(max)];
+
+    let mut matches = vec![];
+
+    // middle
+    let chunk_size = (middle.len()
+        / std::thread::available_parallelism().unwrap_or(std::num::NonZeroUsize::new(1).unwrap()))
+    .max(1);
+    let chunks: Vec<_> = middle.chunks(chunk_size).enumerate().collect();
+    matches.append(
+        &mut chunks
+            .par_iter()
+            .map(|(index, chunk)| {
+                let mut matches = vec![];
+                let offset = index * chunk_size;
+
+                for (i, _) in chunk.iter().enumerate() {
+                    let j = offset + i;
+                    for (id, p) in &bins[data[j] as usize] {
+                        if p.is_match(data, j) {
+                            matches.push((*id, p.compute_result(data, base_address, j)));
+                        }
+                    }
+                }
+                matches
+            })
+            .flatten()
+            .collect(),
+    );
+
+    // suffix
+    let start = middle.len();
+    for (id, p) in patterns {
+        for i in start..start + (data.len() - middle.len()).saturating_sub(p.sig.len() - 1) {
+            if p.is_match(data, i) {
+                matches.push((*id, base_address + i));
+            }
+        }
+    }
+
+    matches
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -140,8 +460,33 @@ mod test {
     }
 
     #[test]
+    fn test_scan_lookup() {
+        test_scan_algo(scan_lookup);
+    }
+
+    #[test]
     fn test_scan_memchr() {
         test_scan_algo(scan_memchr);
+    }
+
+    #[test]
+    fn test_scan_memchr_lookup() {
+        test_scan_algo(scan_memchr_lookup);
+    }
+
+    #[test]
+    fn test_scan_simple() {
+        test_scan_algo(scan_simple);
+    }
+
+    #[test]
+    fn test_scan_simple_batched() {
+        test_scan_algo(scan_simple_batched);
+    }
+
+    #[test]
+    fn test_scan_simple_batched_lookup() {
+        test_scan_algo(scan_simple_batched_lookup);
     }
 
     fn test_scan_algo(scan: ScanFn) {
@@ -180,5 +525,24 @@ mod test {
                 .collect::<Vec<_>>();
             assert_eq!(matches[(3 - (i % 3)) % 3], res);
         }
+    }
+
+    #[test]
+    fn fsd() {
+        let data = std::fs::read("games/FSD/FSD-Win64-Shipping.exe").unwrap();
+
+        let raw_patterns = crate::patterns::get_patterns()
+            .unwrap()
+            .into_iter()
+            .map(|c| c.pattern)
+            .collect::<Vec<_>>();
+        let id_patterns = raw_patterns.iter().map(|p| (&(), p)).collect::<Vec<_>>();
+
+        let p = id_patterns;
+        scan(&p, 0, &data);
+        scan_memchr(&p, 0, &data);
+        scan_simple(&p, 0, &data);
+        scan_simple_batched(&p, 0, &data);
+        scan_simple_batched_lookup(&p, 0, &data);
     }
 }
