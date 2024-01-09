3369b571c77c54535ea3c00a25651c099c28aaab
Truman Kilen <t@kilen.me>
2024-01-08 19:11:25 -0600

    Add FEngineLoopInit resolver

--- patternsleuth/src/resolvers/unreal/game_loop.rs
+++ patternsleuth/src/resolvers/unreal/game_loop.rs
@@ -4,7 +4,7 @@ use futures::future::join_all;
 
 use patternsleuth_scanner::Pattern;
 
-use crate::resolvers::{ensure_one, impl_resolver_singleton, Result};
+use crate::resolvers::{ensure_one, impl_resolver_singleton, unreal::util, Result};
 
 #[derive(Debug, PartialEq)]
 #[cfg_attr(
@@ -12,7 +12,6 @@ use crate::resolvers::{ensure_one, impl_resolver_singleton, Result};
     derive(serde::Serialize, serde::Deserialize)
 )]
 pub struct UGameEngineTick(pub usize);
-
 impl_resolver_singleton!(UGameEngineTick, |ctx| async {
     let strings = ctx
         .scan(Pattern::from_bytes(b"EngineTickMisc\x00".to_vec()).unwrap())
@@ -35,4 +34,32 @@ impl_resolver_singleton!(UGameEngineTick, |ctx| async {
         .flatten();
 
     Ok(UGameEngineTick(ensure_one(fns)?))
+});
+
+/// int32_t FEngineLoop::Init(class FEngineLoop* this)
+#[derive(Debug, PartialEq)]
+#[cfg_attr(
+    feature = "serde-resolvers",
+    derive(serde::Serialize, serde::Deserialize)
+)]
+pub struct FEngineLoopInit(pub usize);
+impl_resolver_singleton!(FEngineLoopInit, |ctx| async {
+    let search_strings = [
+        "FEngineLoop::Init\0",
+        "Failed to load UnrealEd Engine class '%s'.",
+        "One or more modules failed PostEngineInit",
+    ];
+    let strings = join_all(
+        search_strings
+            .into_iter()
+            .map(|s| ctx.scan(util::utf16_pattern(s))),
+    )
+    .await
+    .into_iter()
+    .flatten()
+    .collect::<Vec<_>>();
+
+    let refs = util::scan_xrefs(ctx, &strings).await;
+    let fns = util::root_functions(ctx, &refs)?;
+    Ok(Self(ensure_one(fns)?))
 });
