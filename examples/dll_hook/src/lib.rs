10c9aa26936c3d2b96806d8dc18fcb5f2dab8876
Truman Kilen <t@kilen.me>
2024-01-12 21:20:30 -0600

    Remove demo recording stuff

--- examples/dll_hook/src/lib.rs
+++ examples/dll_hook/src/lib.rs
@@ -101,32 +101,6 @@ fn setup() -> Result<PathBuf> {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct StartRecordingReplay(usize);
-type FnStartRecordingReplay = unsafe extern "system" fn(
-    this: *const ue::UObject, // game instance
-    name: &ue::FString,
-    friendly_name: &ue::FString,
-    additional_options: &ue::TArray<ue::FString>,
-    analytics_provider: ue::TSharedPtr<c_void>,
-);
-impl StartRecordingReplay {
-    fn get(&self) -> FnStartRecordingReplay {
-        unsafe { std::mem::transmute(self.0) }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct StopRecordingReplay(usize);
-type FnStopRecordingReplay = unsafe extern "system" fn(
-    this: *const ue::UObject, // game instance
-);
-impl StopRecordingReplay {
-    fn get(&self) -> FnStopRecordingReplay {
-        unsafe { std::mem::transmute(self.0) }
-    }
-}
-
-#[derive(Debug, PartialEq)]
 pub struct FFrameKismetExecutionMessage(usize);
 
 mod resolvers {
@@ -136,28 +110,6 @@ mod resolvers {
         resolvers::{futures::future::join_all, *},
         scanner::Pattern,
     };
-
-    impl_resolver!(StartRecordingReplay, |ctx| async {
-        // public: virtual void __cdecl UGameInstance::StartRecordingReplay(class FString const &, class FString const &, class TArray<class FString, class TSizedDefaultAllocator<32> > const &, class TSharedPtr<class IAnalyticsProvider, 0>)
-        let patterns = [
-            "48 89 5C 24 08 48 89 6C 24 10 48 89 74 24 18 48 89 7C 24 20 41 56 48 83 EC 40 49 8B F1 49 8B E8 4C 8B F2 48 8B F9 E8 ?? ?? ?? 00 48 8B D8 48 85 C0 74 24 E8 ?? ?? ?? 00 48 85 C0 74 1A 4C 8D 48 ?? 48 63 40 ?? 3B 43 ?? 7F 0D 48 8B C8 48 8B 43 ?? 4C 39 0C C8 74 02 33 DB 48 8D 8F ?? 00 00 00 48 8B D3 E8"
-        ];
-
-        let res = join_all(patterns.iter().map(|p| ctx.scan(Pattern::new(p).unwrap()))).await;
-
-        Ok(Self(ensure_one(res.into_iter().flatten())?))
-    });
-
-    impl_resolver!(StopRecordingReplay, |ctx| async {
-        // public: virtual void __cdecl UGameInstance::StopRecordingReplay(void)
-        let patterns = [
-            "48 89 5C 24 08 57 48 83 EC 20 48 8B F9 E8 ?? ?? ?? 00 48 8B D8 48 85 C0 74 24 E8 ?? ?? ?? 00 48 85 C0 74 1A 48 8D 50 ?? 48 63 40 ?? 3B 43 ?? 7F 0D 48 8B C8 48 8B 43 ?? 48 39 14 C8 74 02 33 DB 48 8D 8F ?? 00 00 00 48 8B D3 E8 ?? ?? ?? 00 48 85 C0 74 08 48 8B C8 E8 ?? ?? ?? 00 48 8B 5C 24 30 48 83 C4"
-        ];
-
-        let res = join_all(patterns.iter().map(|p| ctx.scan(Pattern::new(p).unwrap()))).await;
-
-        Ok(Self(ensure_one(res.into_iter().flatten())?))
-    });
 
     impl_resolver_singleton!(FFrameKismetExecutionMessage, |ctx| async {
         // void FFrame::KismetExecutionMessage(wchar16 const* Message, enum ELogVerbosity::Type Verbosity, class FName WarningId)
@@ -170,8 +122,6 @@ mod resolvers {
 impl_try_collector! {
     #[derive(Debug, PartialEq, Clone)]
     struct DllHookResolution {
-        start_recording_replay: StartRecordingReplay,
-        stop_recording_replay: StopRecordingReplay,
         gmalloc: GMalloc,
         guobject_array: GUObjectArray,
         fnametostring: FNameToString,
