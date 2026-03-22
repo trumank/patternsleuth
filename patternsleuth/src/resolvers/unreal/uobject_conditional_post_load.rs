90327cade1dce62f07c97d9b672f514d02c9f810
Archengius <Archengius@gmail.com>
2026-03-22 17:13:28 +0100

    Testing additional resolvers

--- patternsleuth/src/resolvers/unreal/uobject_conditional_post_load.rs
+++ patternsleuth/src/resolvers/unreal/uobject_conditional_post_load.rs
@@ -5,27 +5,40 @@ use iced_x86::{Decoder, DecoderOptions, Instruction, Mnemonic, OpKind, Register}
 use itertools::Itertools;
 use patternsleuth_scanner::Pattern;
 
-fn is_scratch_register(register: Register) -> bool {
+fn is_volatile_register(register: Register) -> bool {
     register == Register::RAX
-        && register == Register::RCX
-        && register == Register::RDX
-        && register == Register::R8
-        && register == Register::R9
+        || register == Register::RCX
+        || register == Register::RDX
+        || register == Register::R8
+        || register == Register::R9
 }
 
-fn match_postloading_assignments_tail(instructions: &Vec<Instruction>, local_index: &mut usize, currently_post_loaded_object_by_alt: &Option<(Register, u32)>, lea_storage_register: Register, object_source_register: Register) -> bool {
-    // Optional: mov qword [rdi+0x90], $zeroed_register
-    if *local_index + 1 < instructions.len() && instructions[*local_index].mnemonic() == Mnemonic::Mov && instructions[*local_index].op0_kind() == OpKind::Memory && instructions[*local_index].op1_kind() == OpKind::Register && let Some((base_register, displacement)) = currently_post_loaded_object_by_alt && instructions[*local_index].memory_base() == *base_register && instructions[*local_index].memory_displacement32() == *displacement {
-        *local_index += 1;
-    }
-
+fn match_postloading_assignments_tail(instructions: &Vec<Instruction>, local_index: &mut usize, lea_storage_register: Register, object_source_register: Register) -> bool {
     // mov qword [rdi+0x90], $object_source_register
-    if *local_index + 1 < instructions.len() && instructions[*local_index].mnemonic() == Mnemonic::Mov && instructions[*local_index].op0_kind() == OpKind::Memory && instructions[*local_index].op1_kind() == OpKind::Register && instructions[*local_index].op1_register() == object_source_register {
+    if *local_index + 1 < instructions.len() && instructions[*local_index].mnemonic() == Mnemonic::Mov && instructions[*local_index].op0_kind() == OpKind::Memory &&
+        instructions[*local_index].op1_kind() == OpKind::Register && instructions[*local_index].op1_register() == object_source_register {
         let async_package_register = instructions[*local_index].memory_base();
         *local_index += 1;
 
         // mov qword [rdi+0x98], $lea_storage_register
-        if !is_scratch_register(async_package_register) && instructions[*local_index].mnemonic() == Mnemonic::Mov && instructions[*local_index].op0_kind() == OpKind::Memory && instructions[*local_index].op1_kind() == OpKind::Register && instructions[*local_index].op1_register() == lea_storage_register && instructions[*local_index].memory_base() == async_package_register {
+        if !is_volatile_register(async_package_register) && instructions[*local_index].mnemonic() == Mnemonic::Mov && instructions[*local_index].op0_kind() == OpKind::Memory &&
+            instructions[*local_index].op1_kind() == OpKind::Register && instructions[*local_index].op1_register() == lea_storage_register &&
+            instructions[*local_index].memory_base() == async_package_register {
+            return true;
+        }
+    }
+
+    // Permutation with $lea_storage_register being read first. Observed commonly together with inline lea
+    // mov qword [rdi+0x98], $lea_storage_register
+    if *local_index + 1 < instructions.len() && instructions[*local_index].mnemonic() == Mnemonic::Mov && instructions[*local_index].op0_kind() == OpKind::Memory &&
+        instructions[*local_index].op1_kind() == OpKind::Register && instructions[*local_index].op1_register() == lea_storage_register {
+        let async_package_register = instructions[*local_index].memory_base();
+        *local_index += 1;
+
+        // mov qword [rdi+0x90], $object_source_register
+        if !is_volatile_register(async_package_register) && instructions[*local_index].mnemonic() == Mnemonic::Mov && instructions[*local_index].op0_kind() == OpKind::Memory &&
+            instructions[*local_index].op1_kind() == OpKind::Register && instructions[*local_index].op1_register() == object_source_register &&
+            instructions[*local_index].memory_base() == async_package_register {
             return true;
         }
     }
@@ -45,101 +58,113 @@ impl_resolver_singleton!(all, UObjectConditionalPostLoad, |ctx| async {
         ctx.scan(util::utf16_pattern("postloading_async\0")) // 4.8+
     ]).await.into_iter().flatten().collect_vec();
 
-    #[derive(PartialEq, Eq)] enum V { Indirect, Direct }
     let patterns = strings.into_iter().flat_map(|str_addr| [
-        (V::Indirect, format!("48 8d ?? X0x{str_addr:08x}")),
-        (V::Indirect, format!("4c 8d ?? X0x{str_addr:08x}")),
-        (V::Direct, format!("48 8b cf 49 89 7e ?? e8 | ?? ?? ?? ?? 4d 89 6e ?? 48 8d 05 X0x{str_addr:08x}")),
+        format!("48 8d ?? X0x{str_addr:08x}"),
+        format!("4c 8d ?? X0x{str_addr:08x}"),
     ].into_iter()).collect_vec();
-    let match_result = join_all(patterns.iter().map(|(v, p)| ctx.scan_tagged(v, Pattern::new(p).unwrap()))).await;
+    let matched_addresses = join_all(patterns.iter().map(|v| ctx.scan(Pattern::new(v).unwrap()))).await.into_iter().flatten().collect_vec();
 
     let mut conditional_post_load_addresses = Vec::new();
 
-    for (v, _, addresses) in match_result {
-        for a in addresses {
-            if *v == V::Indirect {
-                let function_disassembly_length = 0x260; // 600 bytes should be enough for that function even when AtomicSetFlags is present
-                let bytes = ctx.image().memory.range(a..a + function_disassembly_length)?;
-                let instructions = Decoder::with_ip(64, bytes, a, DecoderOptions::NONE).iter().collect_vec();
-                let mut candidates: Vec<(Register, u64, Option<(Register, u32)>)> = Vec::new();
+    for matched_address in matched_addresses {
+        let maybe_function_start_address = ctx.image().get_root_function(matched_address)?.map(|f| f.range.start);
+        if maybe_function_start_address.is_none() { continue }
+        let function_start_address = maybe_function_start_address.unwrap();
 
-                let mut index = 0;
-                let lea_instruction = instructions[index];
-                index += 1;
-                assert_eq!(lea_instruction.mnemonic(), Mnemonic::Lea);
-                let lea_storage_register = lea_instruction.op0_register();
+        let function_disassembly_length = 0x500; // 1 KB should be enough for that function even when AtomicSetFlags and debug asserts present
+        let bytes = ctx.image().memory.range(function_start_address..function_start_address + function_disassembly_length)?;
+        let instructions = Decoder::with_ip(64, bytes, function_start_address, DecoderOptions::NONE).iter().collect_vec();
+        let mut candidates: Vec<(Register, u64)> = Vec::new();
+        let mut index = 0;
+        let mut persistent_lea_storage_register: Option<Register> = None;
 
-                // Lea must not relate to a temporary register for the indirect pattern to make sense
-                if !is_scratch_register(lea_storage_register) {
-                    while index < instructions.len() {
-                        let mut local_index = index;
+        while index < instructions.len() {
+            let mut local_index = index;
 
-                        // We are looking for something similar to this here:
-                        // mov rcx, rbx
-                        if instructions[local_index].mnemonic() == Mnemonic::Mov {
-                            if local_index + 1 < instructions.len() && instructions[local_index].op0_kind() == OpKind::Register &&
-                            instructions[local_index].op0_register() == Register::RCX && instructions[local_index].op1_kind() == OpKind::Register {
-                                let object_source_register = instructions[local_index].op1_register();
-                                local_index += 1;
+            // We are looking for something similar to this here:
+            // mov rcx, rbx
+            if local_index + 1 < instructions.len() && instructions[local_index].mnemonic() == Mnemonic::Mov &&
+            instructions[local_index].op0_kind() == OpKind::Register && instructions[local_index].op0_register() == Register::RCX &&
+            instructions[local_index].op1_kind() == OpKind::Register {
+                let object_source_register = instructions[local_index].op1_register();
+                local_index += 1;
 
-                                // Some UE versions (most of them actually) have CurrentlyPostLoadedObjectByALT being set before and after the call
-                                // mov qword [mem], $object_source_register
-                                let mut currently_post_loaded_object_by_alt: Option<(Register, u32)> = None;
-                                if local_index + 1 < instructions.len() && instructions[local_index].mnemonic() == Mnemonic::Mov && instructions[local_index].op0_kind() == OpKind::Memory &&
-                                instructions[local_index].op1_kind() == OpKind::Register &&
-                                    instructions[local_index].op1_register() == object_source_register {
-                                    currently_post_loaded_object_by_alt = Some((instructions[local_index].memory_base(), instructions[local_index].memory_displacement32()));
-                                    local_index += 1;
-                                }
+                // Some UE versions (most of them actually) have CurrentlyPostLoadedObjectByALT being set before and after the call
+                // mov qword [mem], $object_source_register
+                if local_index + 1 < instructions.len() && instructions[local_index].mnemonic() == Mnemonic::Mov && instructions[local_index].op0_kind() == OpKind::Memory &&
+                instructions[local_index].op1_kind() == OpKind::Register && instructions[local_index].op1_register() == object_source_register {
+                    local_index += 1;
+                }
 
-                                // call UObject::ConditionalPostLoad
-                                if local_index + 1 < instructions.len() && instructions[local_index].mnemonic() == Mnemonic::Call &&
-                                instructions[local_index].op0_kind() == OpKind::NearBranch64 && !is_scratch_register(object_source_register) {
-                                    let call_target_address = instructions[local_index].near_branch64();
-                                    local_index += 1;
+                // call UObject::ConditionalPostLoad
+                if local_index + 1 < instructions.len() && instructions[local_index].mnemonic() == Mnemonic::Call &&
+                instructions[local_index].op0_kind() == OpKind::NearBranch64 && !is_volatile_register(object_source_register) {
+                    let call_target_address = instructions[local_index].near_branch64();
+                    local_index += 1;
 
-                                    // Try to match the tail now
-                                    if match_postloading_assignments_tail(&instructions, &mut local_index, &currently_post_loaded_object_by_alt, lea_storage_register, object_source_register) {
-                                        // We matched the tail, this is UObject::ConditionalPostLoad
-                                        conditional_post_load_addresses.push(call_target_address);
-                                        break;
-                                    } else {
-                                        // In 4.24-4.25, we will encounter AtomicallyClearInternalFlags(EInternalObjectFlags::AsyncLoading) call here before our expected assignments
-                                        // It can be easily distinguished by a 4-byte read from the object pointer to eax
-                                        // mov eax, dword [rsi+InternalIndex]
-                                        if instructions[local_index].mnemonic() == Mnemonic::Mov && instructions[local_index].op0_kind() == OpKind::Register &&
-                                        instructions[local_index].op0_register().size() == 4 && instructions[local_index].op1_kind() == OpKind::Memory && instructions[local_index].memory_base() == object_source_register {
-                                            // Write down information about a possible later tail and keep processing instructions
-                                            candidates.push((object_source_register, call_target_address, currently_post_loaded_object_by_alt));
-                                            index = local_index;
-                                            continue;
-                                        }
-                                    }
-                                }
-                            } else if instructions[local_index].op0_kind() == OpKind::Memory {
-                                // Try to match this as a tail against all possible candidates
-                                let mut should_break_from_outer_loop = false;
-                                for candidate in &candidates {
-                                    if match_postloading_assignments_tail(&instructions, &mut local_index, &candidate.2, lea_storage_register, candidate.0) {
-                                        // We matched the tail for one of the candidates
-                                        conditional_post_load_addresses.push(candidate.1);
-                                        should_break_from_outer_loop = true;
-                                        break;
-                                    }
-                                }
-                                if should_break_from_outer_loop {
-                                    break;
-                                }
-                            }
-                        }
+                    // We have matched a potential ConditionalPostLoad candidate, we just need to associate it with a lea
+                    // That could happen immediately in the same iteration (if there are no extra instructions in-between) or with some instructions
+                    // between the call and the tail (development builds, UE 4.25 builds and other cases)
+                    index = local_index;
+                    candidates.push((object_source_register, call_target_address));
+                }
+            }
 
-                        // Advance the instruction pointer
-                        index += 1;
+            let mut try_match_tails_against_lea_register: Option<Register> = persistent_lea_storage_register;
+
+            // Try to match inline lea or persistent lea instruction
+            // lea $destination_register, [rip+0x00]
+            if instructions[local_index].mnemonic() == Mnemonic::Lea && instructions[local_index].op0_kind() == OpKind::Register &&
+            instructions[local_index].op1_kind() == OpKind::Memory && instructions[local_index].is_ip_rel_memory_operand() {
+                let lea_instruction_address = instructions[local_index].ip();
+                let lea_destination_register = instructions[local_index].op0_register();
+
+                if lea_instruction_address == matched_address {
+                    // This is the lea by which we have identified this function. This could be a persistent lea or inline lea, depending on whenever
+                    // destination register is a volatile register or not
+                    local_index += 1;
+
+                    if is_volatile_register(lea_destination_register) {
+                        // This is a volatile lea, do not store its destination as persistent_lea_storage_register, only try to match against it this iteration
+                        try_match_tails_against_lea_register = Some(lea_destination_register);
+                    } else {
+                        // This is a lea with a persistent register, we want to match all tails against it from now on
+                        persistent_lea_storage_register = Some(lea_destination_register);
+                        try_match_tails_against_lea_register = persistent_lea_storage_register;
+                        index = local_index;
                     }
                 }
-            } else {
-                conditional_post_load_addresses.push(ctx.image().memory.rip4(a)?);
             }
+
+            // If we have a known lea register to match tails against, try to do so
+            if let Some(lea_register) = try_match_tails_against_lea_register && instructions[local_index].mnemonic() == Mnemonic::Mov &&
+            instructions[local_index].op0_kind() == OpKind::Memory {
+                // Try to match this as a tail against all possible candidates
+                let mut should_break_from_outer_loop = false;
+
+                // Have to iterate in reverse, otherwise we may match another function call on UObject (for example, in development builds, we could match
+                // UObject::GetStatID coming from FScopeCycleCounterUObject since it will precede a call to UObject::ConditionalPostLoad)
+                for candidate in candidates.iter().rev() {
+                    let mut local_index_copy = local_index;
+                    if match_postloading_assignments_tail(&instructions, &mut local_index_copy, lea_register, candidate.0) {
+                        // We matched the tail for one of the candidates
+                        conditional_post_load_addresses.push(candidate.1);
+                        should_break_from_outer_loop = true;
+                        break;
+                    }
+                }
+                if should_break_from_outer_loop {
+                    break;
+                }
+            }
+
+            // Do not match past function return statement
+            if instructions[index].mnemonic() == Mnemonic::Ret {
+                break;
+            }
+
+            // Advance the instruction pointer
+            index += 1;
         }
     }
 
